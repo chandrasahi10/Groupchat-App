eb46153c1ef4d0f95854ef17d94f3bfe822ecf96
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-07-20 16:57:45 +0530

    Finalised features

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get('/login',(req,res,next)=>{
-    res.send('<form onSubmit = "localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/login" method="POST"><input id="username" type="text" name="name1"></input><button type="submit">Login</button></form>');
+    res.send('<title>Login Page</title><form onSubmit = "localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/login" method="POST"><label>Enter username to login:</label><br><br><input id="username" type="text" name="name" placeholder="Username"></input><br><br><button type="submit">Login</button></form>');
 });
 
 app.post('/login', (req,res,next)=>{
@@ -13,17 +16,33 @@ app.post('/login', (req,res,next)=>{
 });
 
 app.get('/', (req,res,next)=>{
-    res.send('<form action="/" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form>');
+    fs.readFile("user.txt", (err,data) => {
+        if(err){
+            console.log(err);
+            data = 'No chats are available'
+        }
+        res.send(
+            `<title>Chat-App</title><label>Chats:</label><br><br>${data}<hr><form onSubmit = "document.getElementById('username').value = localStorage.getItem('username')" action="/" method="POST">
+               <input type="hidden" name="username" id="username"><br>
+               <label>Enter message to send:</label><br><br>
+               <input type="text" name="message" placeholder="message"><br><br>
+               <button type="submit">Send</button>
+            </form>`
+            );
+    })
+   
 });
 
 app.post('/',(req,res,next)=>{
-    fs.writeFile('message.txt','message',(err)=>{
+    fs.writeFile('user.txt',`${req.body.username}: ${req.body.message} `,{flag: 'a'},(err)=>{
         if(err){
             console.log(err);
+        }else{
+
+            res.redirect('/');
         }
-    res.redirect('/');
+    })
     });
-})
 
 
 app.listen(3000);
