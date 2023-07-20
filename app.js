c27eea78c9cce6b220fb8c9a2c67a9f85929d528
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-07-20 12:36:22 +0530

    Added functionality to store messages into file.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 
+
 app.get('/login',(req,res,next)=>{
-    res.send('<form onSubmit = "localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/login" method="POST"><input id="username" type="text" name="name"></input><button type="submit">Login</button></form>');
+    res.send('<form onSubmit = "localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/login" method="POST"><input id="username" type="text" name="name1"></input><button type="submit">Login</button></form>');
 });
 
 app.post('/login', (req,res,next)=>{
@@ -10,7 +13,16 @@ app.post('/login', (req,res,next)=>{
 });
 
 app.get('/', (req,res,next)=>{
-    res.send('<form><input type="text" name="message"><button type="submit">Send</button></input></form>');
+    res.send('<form action="/" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form>');
+});
+
+app.post('/',(req,res,next)=>{
+    fs.writeFile('message.txt','message',(err)=>{
+        if(err){
+            console.log(err);
+        }
+    res.redirect('/');
+    });
 })
 
 
